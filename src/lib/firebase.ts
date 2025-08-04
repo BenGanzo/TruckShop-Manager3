1c1ca903e462bbc68361ffda75e85d18c94bbf12
Benjamin Ganzo <ventasmonarchtracking@gmail.com>
2025-08-04 14:09:40 +0000

    NO ME DEJA.. Pero te los puedo pegar aqui uno por uno y haces los cambi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // INSTRUCCIONES: Reemplaza este objeto con la configuración de tu propio proyecto de Firebase.
 // Puedes encontrarla en la configuración de tu proyecto en la consola de Firebase.
 const firebaseConfig = {
-  apiKey: "POR_FAVOR_REEMPLAZA_CON_TU_API_KEY",
+  apiKey: "AIzaSyA5P6Jj--CRE4CdFA9tqiPYpSItzxnsr20",
   authDomain: "POR_FAVOR_REEMPLAZA_CON_TU_AUTH_DOMAIN",
   projectId: "POR_FAVOR_REEMPLAZA_CON_TU_PROJECT_ID",
   storageBucket: "POR_FAVOR_REEMPLAZA_CON_TU_STORAGE_BUCKET",
