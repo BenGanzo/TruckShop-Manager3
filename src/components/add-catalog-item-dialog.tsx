5068ec45599757ac22a8faadde4cac4c08bf0973
Benjamin Ganzo <ventasmonarchtracking@gmail.com>
2025-08-08 13:28:54 +0000

    prueba de error parts1.1

--- src/components/add-catalog-item-dialog.tsx
+++ src/components/add-catalog-item-dialog.tsx
@@ -99,6 +99,7 @@ export function AddCatalogItemDialog({ type }: AddCatalogItemDialogProps) {
   const [kitQty, setKitQty] = useState(1);
 
   const onPartSubmit = async (data: z.infer<typeof partSchema>) => {
+    console.log("Enviando estos datos:", { companyId, partData: data });
     if (!companyId) return;
     setIsLoading(true);
     const result = await addCatalogPart(companyId, data);
